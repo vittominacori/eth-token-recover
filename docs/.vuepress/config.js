5c73a851995031c4f28af47d0800a31f35d61d5a
vittominacori <vittoriominacori@gmail.com>
2020-07-01 00:06:28 +0200

    update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,11 @@ module.exports = {
   title: 'Recover any ERC20 token lost in your contract',
   description: 'TokenRecover allows the contract owner to recover any ERC20 token sent into the contract for error.',
   base: '/eth-token-recover/',
-  ga: 'UA-115756440-2',
+  plugins: [
+    ['@vuepress/google-analytics', {
+      ga: 'UA-115756440-2',
+    }],
+  ],
   themeConfig: {
     repo: 'vittominacori/eth-token-recover',
     sidebar: 'auto',
