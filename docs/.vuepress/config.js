2b5d9067a5be241f294d3dc4b7df62c67c8b530d
vittominacori <vittoriominacori@gmail.com>
2021-05-01 15:54:07 +0200

    chore: update openzeppelin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,12 @@
+const vars = require('./.env.json');
+
 module.exports = {
   title: 'Recover any ERC20 token lost in your contract',
   description: 'TokenRecover allows the contract owner to recover any ERC20 token sent into the contract for error.',
   base: '/eth-token-recover/',
   plugins: [
     ['@vuepress/google-analytics', {
-      ga: 'UA-115756440-2',
+      ga: vars.gaId,
     }],
   ],
   head: [
