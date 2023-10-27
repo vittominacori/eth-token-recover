b68fe4aef65c686d8a1056f1a5b6e951d84d9a64
Vittorio Minacori <vittoriominacori@gmail.com>
2023-10-27 15:50:27 +0200

    feat: update page metadata

--- pages/.vuepress/config.js
+++ pages/.vuepress/config.js
@@ -1,8 +1,14 @@
 const vars = require('./.env.json');
 
+const title = 'ETH Token Recover | Recover any ERC20 or NFT (ERC721) Token';
+const description =
+  'TokenRecover allows to recover any ERC20 or NFT (ERC721) token sent into the contract and send them to a receiver.';
+const url = 'https://vittominacori.github.io/eth-token-recover';
+const image = '';
+
 module.exports = {
-  title: 'Recover any ERC20 token lost in your contract',
-  description: 'TokenRecover allows the contract owner to recover any ERC20 token sent into the contract for error.',
+  title: 'Recover any ERC20 or NFT (ERC721) Token',
+  description: description,
   base: '/eth-token-recover/',
   plugins: [
     [
@@ -12,7 +18,21 @@ module.exports = {
       },
     ],
   ],
-  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'title', property: 'og:title', content: title }],
+    ['meta', { name: 'description', property: 'og:description', content: description }],
+    ['meta', { name: 'image', property: 'og:image', content: image }],
+    ['meta', { property: 'og:title', content: title }],
+    ['meta', { property: 'og:description', content: description }],
+    ['meta', { property: 'og:image', content: image }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:url', content: url }],
+    ['meta', { property: 'twitter:title', content: title }],
+    ['meta', { property: 'twitter:description', content: description }],
+    ['meta', { property: 'twitter:image', content: image }],
+    ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
+  ],
   themeConfig: {
     repo: 'vittominacori/eth-token-recover',
     sidebar: 'auto',
